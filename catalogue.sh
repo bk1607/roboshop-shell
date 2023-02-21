9a0c7a2a03a4c26afed8c6d0c782929bf50c19eb
BALA KISHORE ANUMALA <baluanumala16@gmail.com>
2023-02-21 15:11:47 +0530

    catalogue server

---
+++ catalogue.sh
@@ -0,0 +1,61 @@
+source common.sh
+
+print_head "setting up nodejs repos"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>"${log_file}"
+error_check $?
+
+print_head "installing nodejs"
+yum install nodejs -y &>>"${log_file}"
+error_check $?
+
+print_head "add application user"
+id roboshop &>>"${log_file}"
+if [ $? -ne 0 ];then
+  useradd roboshop &>>"${log_file}"
+fi
+error_check $?
+
+print_head "creating app directory"
+if [ ! -d /app ];then
+  mkdir /app &>>"${log_file}"
+fi
+error_check $?
+
+print_head "Downloading catalogue files"
+curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>"${log_file}"
+error_check $?
+
+print_head "change to app directory"
+cd /app
+error_check $?
+
+print_head "unzipping the file"
+unzip /tmp/catalogue.zip
+error_check $?
+
+print_head "install nodejs dependencies"
+npm install
+error_check $?
+
+print_head "copying catalogue service file"
+cp "${code_dir}"/configs/catalogue.service /etc/systemd/system/catalogue.service &>>"${log_file}"
+error_check $?
+
+print_head "copying mongodb repo file "
+cp "${code_dir}"/configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>"${log_file}"
+error_check $?
+
+print_head "installing mongodb"
+yum install mongodb-org-shell -y &>>"${log_file}"
+error_check $?
+
+print_head "loading the schema"
+mongo --host mongodb.devops2023.online </app/schema/catalogue.js &>>"${log_file}"
+error_check $?
+
+
+print_head " do daemon-reload and restart the catalogue service "
+systemctl daemon-reload
+systemctl enable catalogue
+systemctl restart catalogue
+error_check $?
--- common.sh
+++ common.sh
@@ -5,3 +5,13 @@ rm -f ${log_file}
 print_head(){
   echo -e "\e[35m$1 \e[0m"
 }
+
+error_check(){
+  if [ $1 -eq 0 ]; then
+    echo SUCCESS
+  else
+    echo FAILURE
+    echo READ the log file ${log_file} for more information about error_check
+    exit 1
+  fi
+}
