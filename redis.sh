abefc77701a749d0145f80347bd6b60fe6a51f48
BALA KISHORE ANUMALA <baluanumala16@gmail.com>
2023-02-21 22:46:26 +0530

    redis.sh

--- redis.sh
+++ redis.sh
@@ -13,11 +13,11 @@ yum install redis -y &>>${log_file}
 error_check $?
 
 print_head "Changing IP address in config file"
-sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis.conf &>>${log_file}
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>${log_file}
 error_check $?
 
 print_head "Enable and restart redis service"
 systemctl enable redis &>>${log_file}
 error_check $?
 systemctl restart redis &>>${log_file}
-
+error_check $?
