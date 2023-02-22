2ca668580d5077ad93948cf9203fcb05314fb736
BALA KISHORE ANUMALA <baluanumala16@gmail.com>
2023-02-22 14:22:14 +0530

    node

--- catalogue.sh
+++ catalogue.sh
@@ -1,43 +1,6 @@
 source common.sh
+component = 'catalogue'
 
-print_head "setting up nodejs repos"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>"${log_file}"
-error_check $?
-
-print_head "installing nodejs"
-yum install nodejs -y &>>"${log_file}"
-error_check $?
-
-print_head "add application user"
-id roboshop &>>"${log_file}"
-if [ $? -ne 0 ];then
-  useradd roboshop &>>"${log_file}"
-fi
-error_check $?
-
-print_head "creating app directory"
-if [ ! -d /app ];then
-  mkdir /app &>>"${log_file}"
-elif [ -d /app ];then
-  rm -rf /app/* &>>${log_file}
-fi
-error_check $?
-
-print_head "Downloading catalogue files"
-curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>"${log_file}"
-error_check $?
-
-print_head "change to app directory"
-cd /app
-error_check $?
-
-print_head "unzipping the file"
-unzip /tmp/catalogue.zip &>>${log_file}
-error_check $?
-
-print_head "install nodejs dependencies"
-npm install &>>$"{log_file}"
-error_check $?
 
 print_head "copying catalogue service file"
 cp "${code_dir}"/configs/catalogue.service /etc/systemd/system/catalogue.service &>>"${log_file}"
--- common.sh
+++ common.sh
@@ -15,3 +15,44 @@ error_check(){
     exit 1
   fi
 }
+
+node_js(){
+  print_head "setting up nodejs repos"
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>"${log_file}"
+  error_check $?
+
+  print_head "installing nodejs"
+  yum install nodejs -y &>>"${log_file}"
+  error_check $?
+
+  print_head "add application user"
+  id roboshop &>>"${log_file}"
+  if [ $? -ne 0 ];then
+    useradd roboshop &>>"${log_file}"
+  fi
+  error_check $?
+
+  print_head "creating app directory"
+  if [ ! -d /app ];then
+    mkdir /app &>>"${log_file}"
+  elif [ -d /app ];then
+    rm -rf /app/* &>>${log_file}
+  fi
+  error_check $?
+
+  print_head "Downloading catalogue files"
+  curl -L -o /tmp/"${component}.zip" https://roboshop-artifacts.s3.amazonaws.com/"${component}.zip" &>>"${log_file}"
+  error_check $?
+
+  print_head "change to app directory"
+  cd /app
+  error_check $?
+
+  print_head "unzipping the file"
+  unzip /tmp/"${component}.zip" &>>${log_file}
+  error_check $?
+
+  print_head "install nodejs dependencies"
+  npm install &>>$"{log_file}"
+
+}
