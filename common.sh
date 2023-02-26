269bff40d4089eed6ccb5bd65d91154679ce73ae
BALA KISHORE ANUMALA <baluanumala16@gmail.com>
2023-02-26 10:26:35 +0530

    mongod

--- common.sh
+++ common.sh
@@ -8,7 +8,7 @@ code_dir=$(pwd)
 
 error_check(){
   if [ $1 -eq 0 ];then
-    echo -e "\e[35mSuccess\e[0m"
+    echo -e "\e[33mSuccess\e[0m"
   else
     echo -e "\e[31mSomething went wrong check ${log_file} for the error details\e[0m"
   fi
---
+++ mongodb.sh
@@ -0,0 +1,17 @@
+source common.sh
+print_head "creating repo files"
+cp "${code_dir}"/configs/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>"${log_file}"
+error_check $?
+
+print_head "Installing mongodb"
+yum install mongodb-org -y &>>"${log_file}"
+error_check $?
+
+print_head "Edit the port address "
+sed -e -i "s/127.0.0.1/0.0.0.0" /etc/mongod.conf &>>"${log_file}"
+error_check $?
+
+print_head "Enable and restart the service"
+systemctl enable mongod &>>"${log_file}"
+systemctl start mongod &>>"${log_file}"
+error_check $?
